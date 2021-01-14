fb8d2027dcc7e006b2302d926e912f0829a77324
oskvr <oskar.puustinen@gmail.com>
2021-01-14 20:50:47 +0100

    Configured project for Azure

--- Todo.Core/Startup.cs
+++ Todo.Core/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Todo.Core
 {
@@ -21,9 +22,19 @@ namespace Todo.Core
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseSqlServer(
-					Configuration.GetConnectionString("DefaultConnection")));
+			if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+			{
+				services.AddDbContext<ApplicationDbContext>(options =>
+					options.UseSqlServer(
+						Configuration.GetConnectionString("DefaultConnectionProd")));
+			}
+			else
+			{
+				services.AddDbContext<ApplicationDbContext>(options =>
+					options.UseSqlServer(
+						Configuration.GetConnectionString("DefaultConnection")));
+			}
+			services.BuildServiceProvider().GetService<ApplicationDbContext>().Database.Migrate();
 			services.AddDatabaseDeveloperPageExceptionFilter();
 			services.AddDefaultIdentity<ApplicationUser>(options =>
 			{
@@ -34,33 +45,33 @@ namespace Todo.Core
 			services.AddRazorPages();
 		}
 
-	// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-	{
-		if (env.IsDevelopment())
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			app.UseDeveloperExceptionPage();
-			app.UseMigrationsEndPoint();
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseMigrationsEndPoint();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapRazorPages();
+			});
 		}
-		else
-		{
-			app.UseExceptionHandler("/Error");
-			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-			app.UseHsts();
-		}
-
-		app.UseHttpsRedirection();
-		app.UseStaticFiles();
-
-		app.UseRouting();
-
-		app.UseAuthentication();
-		app.UseAuthorization();
-
-		app.UseEndpoints(endpoints =>
-		{
-			endpoints.MapRazorPages();
-		});
 	}
-}
 }
